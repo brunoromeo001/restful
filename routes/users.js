6f4e2798ccfa834859eac08d9692e9784f0a5621
Bruno M. Cerqueira <brunocerqueira@recbasvr.com.br>
2019-05-16 16:54:43 -0300

    Listando os usuários do banco NeDB

--- routes/users.js
+++ routes/users.js
@@ -8,16 +8,26 @@ let db = new NeDB({
 module.exports = (app)=>{
 
     app.get('/users', (req, res) => {
-        
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        res.json({
-            users:[{
-                name: 'Bruno Romeo',
-                email: 'contato@hbrunoromeo.com.br',
-                id: 1
-            }]
+
+        db.find({}).sort({name:1}).exec((err, users)=>{
+
+            if (err){
+
+                console.log('error: ${err}');
+                res.status(400).json({
+                    error:err
+                })
+            } else{
+                
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.json({
+                    users
+                });
+            }
+
         });
+        
     });    
 
     app.post('/users', (req, res) => {
@@ -26,7 +36,7 @@ module.exports = (app)=>{
 
             if (err){
 
-                console.log('error: ${err}');
+                console.log('error: ${err}'); 
                 res.status(400).json({
                     error: err
                 });
